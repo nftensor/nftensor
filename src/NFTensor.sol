47565c95e1ec931b877bc9040665fea0f2799c8f
0xcacti <0xcacti@gmail.com>
2023-05-20 16:47:19 -0400

    get a little work in before having to do stuffs :(

--- src/NFTensor.sol
+++ src/NFTensor.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.19;
 
 import "solmate/tokens/ERC721.sol";
+import "solmate/tokens/ERC20.sol";
 import "solmate/auth/Owned.sol";
 
 /// @title NFTensor
@@ -16,7 +17,29 @@ contract NFTensor is ERC721, Owned {
     /// @notice The owner address.
     address constant OWNER_ADDRESS = 0xB95777719Ae59Ea47A99e744AfA59CdcF1c410a1;
 
+    /// @notice wTAO address.
+    address constant WTAO_ADDRESS = 0x77E06c9eCCf2E797fd462A92B6D7642EF85b0A44;
+
+    /// @notice The minimum amount of wTAO required to mint an NFT.
+    uint256 constant MINT_PRICE = 1e9;
+
+    /// @ notice The maximum supply of NFTs that can be minted.
+    uint256 constant MAX_SUPPLY = 500;
+
+    /*//////////////////////////////////////////////////////////////
+                               STATE VARIABLES
+    //////////////////////////////////////////////////////////////*/
+
+    /// @notice The tokenID for the next token to be minted.
+    uint256 public tokenID;
+
+    /// @notice The mapping of tokenID to query.
+    mapping(uint256 => string) public queries;
+
     constructor() ERC721("NFTensor", "NFTENSOR") Owned(OWNER_ADDRESS) { }
+
+    /// @notice Mints a token to the sender.
+    function mint() external { }
 
     /*//////////////////////////////////////////////////////////////
                                METADATA
